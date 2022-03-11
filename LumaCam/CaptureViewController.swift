42e8c4ecb8baf9cb523f21386b4fd7e53881e714
Orbitus <orbitus@orbitusrobotics.com>
2022-03-10 18:39:43 -0800

    added todo for some of the app deliverable features

--- LumaCam/CaptureViewController.swift
+++ LumaCam/CaptureViewController.swift
@@ -38,6 +38,27 @@ class CaptureViewController: UIViewController {
         
         self.addCoachingOverlay()
         self.initFocusNode()
+        self.initRawFeatureParticles()
+    }
+    
+    func initRawFeatureParticles() {
+        // In here we take rawFeaturePoints from current arframe and draw a
+        // particle node on ever 100-1000th instance. Add that particle to
+        // the particle system for nodes and make it fly up like a magical fire
+        // under the object This function is a placeholder reminder to do this.
+        //TODO:
+        //Step1: tap and print out the rawFeaturePoints of an ARFrame
+        //Step2: filter by points that are above our horizontal plane
+        //Step3: add a particle node every 100-1000th feature point so we get a
+        //       sparse fire from the particles, akin to magic.
+    }
+    
+    func postCaptureRawFeatureParticles() {
+        // After a successful capture make sure to take the rawFeaturePoints and
+        // have them fly into the screen and onto the preview box so that it looks
+        // like you captured the object in that preview window
+        
+        //TODO: animate particles nodes engine to fly into the preview box.
     }
     
     func resetARSession() {
@@ -60,9 +81,11 @@ class CaptureViewController: UIViewController {
     @IBAction func tapGestureHandler(_ sender: Any) {
         guard appState == .TapToStart else { return }
         
-        //TODO: HideFocustNode and show the capture reticle
+        
+        //TODO: Perform raycast and set an anchor up with our capture
+        //TODO: HideFocusNode and show the capture reticle
         appState = .Started
-    }	
+    }
     
     func hideCoachView() {
         UIView.animate(withDuration: 0.33) { [weak self] in
@@ -72,42 +95,69 @@ class CaptureViewController: UIViewController {
         }
     }
     
+    var arCoachScene: LumaLabsCapture.ARCoachScene?
+    var captureScene: LumaLabsCapture.CaptureScene?
+    
     @IBAction func beginARDemoScene() {
         hideCoachView()
-        let arCoachScene = try! LumaLabsCapture.loadARCoachScene()
-        arView.scene.anchors.append(arCoachScene)
+        arCoachScene = try! LumaLabsCapture.loadARCoachScene()
+        if let arCoachScene = arCoachScene {
+            arView.scene.anchors.append(arCoachScene)
+            //Initialize Actions
+            arCoachScene.actions.takePhoto.onAction = handleTakePhotoAction(_:)
+        }
     }
     
     @IBAction func beginCaptureScene() {
-        let arCoachScene = try! LumaLabsCapture.loadARCoachScene()
-        arView.scene.anchors.append(arCoachScene)
+        hideCoachView()
+        captureScene = try! LumaLabsCapture.loadCaptureScene()
+        if let captureScene = captureScene {
+            arView.scene.anchors.append(captureScene)
+            //Initialize Actions
+            captureScene.actions.takePhoto.onAction = handleTakePhotoAction(_:)
+        }
     }
     
+    func handleTakePhotoAction(_ entity: Entity?) {
+        guard let entity = entity else { return }
+        //Step1: check entity orientation relative to the origin, does the image seem to be pointing at the object
+        //Step2: check if we are too close, show error UI if this is true
+        //Step3: check if we are too far, show error UI if this is true
+        //Step4: play a sound on the programmatic end only if the image was
+        //       successful? sending a notification back to reality engine needs
+        //       us to make 40 individual notification triggers for each mark
+        //       that is later successfully removed. We can save this for when
+        //       you choose to proceed further.
+        // For now we just allow the reality file to mark the image as take successfully
+        print("didTakePhotoAcrtion \(entity)")
+    }
 }
 
 
-// MARK: - App Management
+// MARK: - App State Management
 
 extension CaptureViewController {
     
     func startApp() {
         DispatchQueue.main.async { [weak self] in
-            //self.arPortNode.isHidden = true
-            //self.focusNode.isHidden = true
+            //TODO: sync content/focus node
+            //Step1: Hide ContentNode
+            //Step2: Hide focusNode
             self?.appState = .DetectSurface
         }
     }
     
     func resetApp() {
         DispatchQueue.main.async { [weak self] in
-            //self.arPortNode.isHidden = true
-            //self.resetARSession()
+            //TODO: sync content/focus node
+            //Step1: Hide ContentNode
+            //Step2: reset ARSession
             self?.appState = .DetectSurface
         }
     }
 }
 
-//MARK: ARCoachingOverlay
+//MARK: - ARCoachingOverlay
 
 extension CaptureViewController: ARCoachingOverlayViewDelegate {
     func addCoachingOverlay() {
@@ -154,12 +204,28 @@ extension CaptureViewController: ARCoachingOverlayViewDelegate {
     func coachingOverlayViewDidRequestSessionReset(_ coachingOverlayView: ARCoachingOverlayView) {
         self.resetApp()
     }
+}
+
+// MARK: - Focus Node Management
+
+extension CaptureViewController {
+    @objc
+    func orientationChanged() {
+        focusPoint = CGPoint(x: view.center.x, y: view.center.y  + view.center.y * 0.1)
+    }
     
-    func begin2DARCoachingInstructions() {
-        //Run this after we show the instructions
-        //beginCaptureScene()
+    func initFocusNode() {
+        //TODO: add the focus reticle scene to the experience and load it here as the focus node
+        let arCoachScene = try! LumaLabsCapture.loadARCoachScene()
+        arView.scene.anchors.append(arCoachScene)
+        
+        focusPoint = CGPoint(x: view.center.x, y: view.center.y + view.center.y * 0.1)
+        NotificationCenter.default.addObserver(self, selector: #selector(CaptureViewController.orientationChanged), name: UIDevice.orientationDidChangeNotification, object: nil)
+        
     }
 }
+
+// MARK: - UIPickerViewDelegate/Datasource
 
 extension CaptureViewController: UIPickerViewDelegate, UIPickerViewDataSource {
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
@@ -191,24 +257,5 @@ extension CaptureViewController: UIPickerViewDelegate, UIPickerViewDataSource {
         default:
             return ""
         }
-    }
-}
-
-// MARK: - Focus Node Management
-
-extension CaptureViewController {
-    @objc
-    func orientationChanged() {
-        focusPoint = CGPoint(x: view.center.x, y: view.center.y  + view.center.y * 0.1)
-    }
-    
-    func initFocusNode() {
-        //TODO: add the focus reticle scene to the experience and load it here as the focus node
-        let arCoachScene = try! LumaLabsCapture.loadARCoachScene()
-        arView.scene.anchors.append(arCoachScene)
-        
-        focusPoint = CGPoint(x: view.center.x, y: view.center.y + view.center.y * 0.1)
-        NotificationCenter.default.addObserver(self, selector: #selector(CaptureViewController.orientationChanged), name: UIDevice.orientationDidChangeNotification, object: nil)
-        
     }
 }
